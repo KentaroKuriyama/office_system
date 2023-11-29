d3f57ea6df0539256af2ce30bef5055b6e510c83
usausa505 <riotshot3@gmail.com>
2023-11-29 16:27:27 +0900

    wip:trouble_report バリデーションメッセージ実装中

--- office_system/app/Http/Controllers/TroubleReportsController.php
+++ office_system/app/Http/Controllers/TroubleReportsController.php
@@ -10,7 +10,7 @@ use Validator;
 
 class TroubleReportsController extends Controller
 {
-    private $foamItems = ['function', 'occurred_at', 'phenomenon', 'reproduction_steps'];
+    private $formItems = ['function', 'occurred_at', 'phenomenon', 'reproduction_steps'];
 
     private $validator = [
         'function' => 'required',
@@ -29,19 +29,61 @@ class TroubleReportsController extends Controller
         return view('trouble_reports.input');
     }
 
-    public function reportPost()
+    /**
+     * 入力画面でのフォーム値を確認画面へ渡す
+     *
+     * @param object $request
+     * @return object
+     */
+    public function reportPost(Request $request) :object
     {
+        $input = $request->only($this->formItems);
 
+        $validator = Validator::make($input, $this->validator);
+
+        if ($validator->fails()) {
+            // actionでクラス名とアクションで可読性を高める
+            return redirect()->action([TroubleReportsController::class, 'reportInput'])
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        // 第一引数をキーとしてセッション変数にフォームの入力値を保存
+        $request->session()->put('trouble_input', $input);
+
+        // 判定に問題が無ければ確認画面にリダリレクト
+        return redirect()->action([TroubleReportsController::class, 'reportConfirm']);
     }
 
-    public function reportConfirm()
+    /**
+     * 確認画面へ遷移
+     *
+     * @param object $request
+     * @return object
+     */
+    public function reportConfirm(Request $request) :object
     {
+        // セッションから値を取り出す
+        $input = $request->session()->get('trouble_input');
 
+        // セッションに値が無ければフォームに戻す
+        if (empty($input)) {
+            return redirect()->action([TroubleReportsController::class, 'reportInput']);
+        }
+
+        return view('trouble_reports.confirm', ['input' => $input]);
     }
 
-    public function reportSend()
+    public function reportSend(Request $request)
     {
+        // セッションから値を取り出す
+        $input = $request->session()->get('trouble_input');
 
+        // 戻るボタンが押されたら入力値と共にフォームにへ戻る
+        if ($request->has('back')) {
+            return redirect()->action([TroubleReportsController::class, 'reportInput'])
+                ->withInput($input);
+        }
     }
 
     public function reportResult()
--- office_system/resources/views/trouble_reports/input.blade.php
+++ office_system/resources/views/trouble_reports/input.blade.php
@@ -8,43 +8,53 @@
     <body>
         @extends('layouts.app')
         @section('content')
-            @if ($errors->any())
-                <div class="error">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
             <div class="container">
                 <div class="main container-fluid">
                     <div class="row bg-light text-dark py-5">
                         <div class="col-md-8 offset-md-2">
                             <h2 class="fs-1 mb-5 text-center fw-bold">障害報告入力画面</h2>
                             <p>以下の項目に必要事項を入力してください</p>
+                            @if ($errors->any())
+                                <div class="rounded bg-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li class="text-white">{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
                             <form method="post" action="{{ route('trouble_report.post') }}">
                                 @csrf
-                                <div class="row">
-                                    <div class="col-md-6 mb-3">
-                                        <h5><b>機能</b></h5>
-                                        <select name="function" class="form-control">
-                                            @foreach(config('const.trouble.function') as $key => $value)
-                                                <option value="{{ $key }}" {{ old('type') == $key ? 'selected' : '' }}>{{ $value }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="col-md-6 mb-3">
-                                        <h5><b>発生日時</b></h5>
-                                        <input type="date" name="occured_at" class="form-control" value="{{ old('occured_at') }}">
-                                    </div>
+                                <div class="col-md-6 mb-3">
+                                    <h5><b>機能</b></h5>
+                                    @error('function')
+                                        <b><span class="text-danger">・{{$message}}</span></b>
+                                    @enderror
+                                    <select name="function" class="form-control">
+                                        @foreach(config('const.trouble.function') as $key => $value)
+                                            <option value="{{ $key }}" {{ old('function') == $key ? 'selected' : '' }}>{{ $value }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <h5><b>発生日時</b></h5>
+                                    @error('occurred_at')
+                                        <b><span class="text-danger">・{{$message}}</span></b>
+                                    @enderror
+                                    <input type="date" name="occurred_at" class="form-control" value="{{ old('occurred_at') }}">
                                 </div>
                                 <div class="mb-4">
                                     <h5><b>現象</b></h5>
+                                    @error('phenomenon')
+                                        <b><span class="text-danger">・{{$message}}</span></b>
+                                    @enderror
                                     <textarea class="form-control" name="phenomenon" rows="5">{{ old('phenomenon') }}</textarea>
                                 </div>
                                 <div class="mb-4">
                                     <h5><b>再現手順</b></h5>
+                                    @error('reproduction_steps')
+                                        <b><span class="text-danger">・{{$message}}</span></b>
+                                    @enderror
                                     <textarea class="form-control" name="reproduction_steps" rows="5">{{ old('reproduction_steps') }}</textarea>
                                 </div>
                                 <div class="text-center pt-4 col-md-6 offset-md-3">
